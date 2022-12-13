e17b34248b7debed8480af856e249ebc9c36ce44
wilpet4 <william.petrik@gmail.com>
2022-12-13 22:16:36 +0100

    alla tester klara, ska städa upp

--- YlvasKaffelager.Tests/UnitTests.cs
+++ YlvasKaffelager.Tests/UnitTests.cs
@@ -11,17 +11,19 @@ namespace YlvasKaffelager.Tests
 {
     public class UnitTests
     {
-        private DbContext context = new DbContext();
+        private IDbContext context;
         private Mock<IDbContext> mock;
+        Coffee firstCoffee;
+        Coffee secondCoffee;
         public UnitTests()
         {
-            Coffee firstCoffee = new Coffee
+            firstCoffee = new Coffee
             {
                 Id = 1,
                 Brand = "Gevalia",
                 Price = 29.90m
             };
-            Coffee secondCoffee = new Coffee
+            secondCoffee = new Coffee
             {
                 Id = 2,
                 Brand = "Lavazza",
@@ -31,20 +33,26 @@ namespace YlvasKaffelager.Tests
             mock = new Mock<IDbContext>();
             mock.Setup(x => x.GetCoffe(1)).Returns(firstCoffee);
             mock.Setup(x => x.GetCoffe(2)).Returns(secondCoffee);
+            context = mock.Object;
         }
-        [Fact]
-        public void Should_Return_Correct_Sum()
+        [Theory]
+        [InlineData(29.90, 1)]
+        [InlineData(49.90, 1)]        
+        [InlineData(29.90, 2)]
+        [InlineData(49.90, 0)]
+        [InlineData(29.90, 5)]
+        [InlineData(-29.90, -2)]
+        public void Should_Return_Correct_Sum(decimal coffeePrice, int orderAmount)
         {
             // Arrange
-            var coffee = context.GetCoffe(1);
             var calculations = new Calculations();
             var withDecorator = new CoffeeDecorator(calculations);
-            decimal expectedWithoutDecorator = 29.90m;
-            decimal expectedWithDecorator = 29.90m * 1.06m;
+            decimal expectedWithoutDecorator = coffeePrice * orderAmount;
+            decimal expectedWithDecorator = (coffeePrice * orderAmount) * 1.06m;
 
             // Act
-            var actualWithoutDecorator = calculations.PriceTotal(coffee.Price, 2);
-            var actualWithDecorator = withDecorator.PriceTotal(coffee.Price, 2);
+            var actualWithoutDecorator = calculations.PriceTotal(coffeePrice, orderAmount);
+            var actualWithDecorator = withDecorator.PriceTotal(coffeePrice, orderAmount);
 
             //Assert
             Assert.Equal(expectedWithoutDecorator, actualWithoutDecorator);
@@ -55,13 +63,15 @@ namespace YlvasKaffelager.Tests
         public void Should_Return_Coffee()
         {
             // Arrange
-
+            var expectedResult = firstCoffee;
 
             // Act
-
+            var actualResult = context.GetCoffe(1);
+            var actualWrongResult = context.GetCoffe(2);
 
             //Assert
-
+            Assert.Equal(expectedResult, actualResult);
+            Assert.NotEqual(expectedResult, actualWrongResult);
         }
     }
 }
